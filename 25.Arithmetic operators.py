93b9b5994551004d7b8759a5fb6604743e9ba77f
Thiru-boss <maran28599@gmail.com>
2024-11-03 19:18:37 +0530

    Add files via upload

---
+++ 25.Arithmetic operators.py
@@ -0,0 +1,33 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=15
+>>> b=4
+>>> c=a+b
+>>> c
+19
+>>> c=a-b
+>>> c
+11
+>>> c=a*b
+>>> c
+60
+>>> c=a**b
+>>> c
+50625
+>>> c=a/b
+>>> c
+3.75
+>>> c=a//b
+>>> c
+3
+>>> c=a%b
+>>> c
+3
+>>> x=21
+>>> y=4
+>>> z=x//y
+>>> z
+5
+>>> z=x%y
+>>> z
+1
---
+++ 27.1.Expression.py
@@ -0,0 +1,6 @@
+length = int (input(' Enter length of a rectangle'))
+breadth = int (input('Enter breath of a rectangle'))
+area = lenght*breadth
+print('area is ',area)
+
+
